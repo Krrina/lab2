29082e17b6bdbe3f0fce3cfacde63cc5532b1ebe
Krrina <106991553+Krrina@users.noreply.github.com>
2022-06-07 11:04:42 +0300

    Create m1.cpp

---
+++ m1/m1.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include<stdlib.h>  
+#include<ctime>   
+int main() {
+	int Z, i, j, N;
+	int *a=(int*)malloc(N*sizeof(int)); 
+	printf("Z=");
+	scanf("%i", &Z);
+	printf("N=");
+	scanf("%d", &N);  
+	srand(time(NULL));
+	j = 0;
+	for (i = 0; i < N; i++) { 
+		a[i] = -100 + rand() % (-100 - 100);
+		printf("a[%d]=%d \n", i, a[i]);
+		if (a[i] > Z) {
+			a[i] = Z;
+			j++;
+			printf("a[%d]=%d \n", i, a[i]); 
+		}
+	}  
+	printf("%i \n", j);   
+}  
